2b2fed67a6d1a3e696189f45c7fd728ec964681f
Christopher <cghurrell@gmail.com>
2017-03-25 19:30:38 +0200

    Update settings.js

--- imports/startup/server/settings.js
+++ imports/startup/server/settings.js
@@ -3,14 +3,7 @@ import '../../api/documents/server/publications.js';
 import Settings from '../../api/documents/settings.js';
 
 if (!Meteor.isProduction) {
-  // const users = [{
-  //   email: 'cghurrell@gmail.com',
-  //   password: 'password',
-  //   profile: {
-  //     name: { first: 'Chris', last: 'Hurrell' },
-  //   },
-  //   roles: ['admin'],
-  // }];
+
   const settings = [
     {
       type: 'General',
@@ -23,9 +16,6 @@ if (!Meteor.isProduction) {
 
   settings.forEach(({ type, app_name, app_logo, app_title, app_subtitle }) => {
 
-
-    // const settings = Settings.findOne();
-    // console.log(settings);
     const settingsExists = Settings.findOne();
 
     if (!settingsExists) {
